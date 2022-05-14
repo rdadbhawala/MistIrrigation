318946ff5a2e288b566191e8e3d335f44785172e
Rushabh Dadbhawala <rdadbhawala@gmail.com>
2022-05-14 22:08:01 +0530

    samples uploaded

--- ArduinoSamples/Blink/Blink.ino
+++ ArduinoSamples/Blink/Blink.ino
@@ -1,12 +1,13 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(13, HIGH);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(2000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);                       // wait for a second
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(3000);                       // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
 }
